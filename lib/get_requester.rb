8eddb68e174599f17d28b82809b2cbda3b787b6c
Matt Thomas <matthew.keith.thomas@gmail.com>
2020-09-25 15:15:52 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -6,9 +6,9 @@ class GetRequester
   URL = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
 
   
-  def initialize(site)
-    @site = site 
-  end 
+  #def initialize(site)
+   # @site = site 
+  #end 
   
   def get_response_body 
     uri = URI.parse(URL)
