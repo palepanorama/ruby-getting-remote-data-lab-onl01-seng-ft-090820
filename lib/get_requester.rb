b01ec1be8f9c04c1c6bed0562ea83ab0c65bbf0a
Matt Thomas <matthew.keith.thomas@gmail.com>
2020-09-25 15:09:40 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -17,7 +17,7 @@ class GetRequester
   end 
   
   def parse_json 
-    programs = JSON.parse(get_response_body)
+    programs = JSON.parse(self.get_response_body)
     programs.collect do |program| 
       program["agency"]
     end 
